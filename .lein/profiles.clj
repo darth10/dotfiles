fd884d9e9fe1482725527aaa6c30110601da60cc
Akhil Wali <akhil.wali.10@gmail.com>
2013-09-29 09:58:17 +0530

    Added lein-ancient

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,4 +1,5 @@
 {:user {:plugins [[lein-ritz "0.7.0"]
+                  [lein-ancient "0.5.0-RC1"]
                   [compojure-app/lein-template "0.2.7"]]
         :dependencies [[ritz/ritz-nrepl-middleware "0.7.0"]]
         :repl-options {:nrepl-middleware
