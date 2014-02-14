7e5346f225bd9b03083356c76bce9240099b21cd
Akhil Wali <akhil.wali.10@gmail.com>
2014-02-14 22:34:56 +0530

    Ritz -> Cider

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
-{:user {:plugins [[lein-ritz "0.7.0"]
-                  [lein-ancient "0.5.0-RC1"]
+{:user {:plugins [[lein-ancient "0.5.0-RC1"]
                   [compojure-app/lein-template "0.2.7"]]
-        :dependencies [[ritz/ritz-nrepl-middleware "0.7.0"]]
+        :dependencies [[cider/cider-nrepl "0.1.0-SNAPSHOT"]]
         :repl-options {:nrepl-middleware
-                       [ritz.nrepl.middleware.javadoc/wrap-javadoc
-                        ritz.nrepl.middleware.simple-complete/wrap-simple-complete]}}}
+                       [cider.nrepl.middleware.doc/wrap-doc
+                        cider.nrepl.middleware.complete/wrap-complete
+                        cider.nrepl.middleware.info/wrap-info]}}}
