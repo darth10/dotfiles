5403284adbfee74d0baf64dfcd70c8c4a1da3d6c
Akhil Wali <akhil.wali.10@gmail.com>
2014-05-05 21:29:27 +0530

    Updated cider-nrepl version and middleware

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,8 +2,11 @@
                   [lein-try "0.4.1"]
                   [lein-exec "0.3.2"]
                   [compojure-app/lein-template "0.4.0"]]
-        :dependencies [[cider/cider-nrepl "0.1.0-SNAPSHOT"]]
+        :dependencies [[cider/cider-nrepl "0.7.0-SNAPSHOT"]]
         :repl-options {:nrepl-middleware
-                       [cider.nrepl.middleware.complete/wrap-complete
+                       [cider.nrepl.middleware.classpath/wrap-classpath
+                        cider.nrepl.middleware.complete/wrap-complete
                         cider.nrepl.middleware.info/wrap-info
-                        cider.nrepl.middleware.inspect/wrap-inspect]}}}
+                        cider.nrepl.middleware.inspect/wrap-inspect
+                        cider.nrepl.middleware.stacktrace/wrap-stacktrace
+                        cider.nrepl.middleware.trace/wrap-trace]}}}
