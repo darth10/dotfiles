234da09bbc45c03cc003115a6019cbe02a0b47bc
Akhil Wali <akhil.wali.10@gmail.com>
2015-07-24 22:24:50 +0530

    Updated cider dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,10 @@
 {:user {:plugins [[lein-ancient "0.5.4"]
                   [lein-try "0.4.1"]
                   [lein-exec "0.3.2"]
-                  [refactor-nrepl "1.1.0-SNAPSHOT"]
+                  [refactor-nrepl "1.2.0-SNAPSHOT"]
                   [http-kit/lein-template "1.0.0-SNAPSHOT"]
                   [compojure-app/lein-template "0.4.0"]]
-        :dependencies [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+        :dependencies [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                        [org.clojure/tools.nrepl "0.2.10"]
                        [compliment "0.1.3"]]
         :repl-options {:nrepl-middleware
