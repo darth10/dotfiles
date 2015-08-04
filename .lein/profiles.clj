c15ccc01da1688b607c5f22d7124d8b097dab6d0
Akhil Wali <akhil.wali.10@gmail.com>
2015-08-04 09:33:03 +0530

    Moved cider to :plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,25 +2,8 @@
                   [lein-try "0.4.1"]
                   [lein-exec "0.3.2"]
                   [refactor-nrepl "1.2.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [http-kit/lein-template "1.0.0-SNAPSHOT"]
                   [compojure-app/lein-template "0.4.0"]]
-        :dependencies [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                       [org.clojure/tools.nrepl "0.2.10"]
-                       [compliment "0.1.3"]]
-        :repl-options {:nrepl-middleware
-                       [cider.nrepl.middleware.apropos/wrap-apropos
-                        cider.nrepl.middleware.classpath/wrap-classpath
-                        cider.nrepl.middleware.complete/wrap-complete
-                        cider.nrepl.middleware.debug/wrap-debug
-                        cider.nrepl.middleware.format/wrap-format
-                        cider.nrepl.middleware.info/wrap-info
-                        cider.nrepl.middleware.inspect/wrap-inspect
-                        cider.nrepl.middleware.macroexpand/wrap-macroexpand
-                        cider.nrepl.middleware.ns/wrap-ns
-                        cider.nrepl.middleware.pprint/wrap-pprint
-                        cider.nrepl.middleware.refresh/wrap-refresh
-                        cider.nrepl.middleware.resource/wrap-resource
-                        cider.nrepl.middleware.stacktrace/wrap-stacktrace
-                        cider.nrepl.middleware.test/wrap-test
-                        cider.nrepl.middleware.trace/wrap-trace
-                        cider.nrepl.middleware.undef/wrap-undef]}}}
+        :dependencies [[org.clojure/tools.nrepl "0.2.10"]
+                       [compliment "0.1.3"]]}}
