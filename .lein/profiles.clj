3e0a1ba87691e8598381c39e7216e1f9057b2953
Akhil Wali <akhil.wali.10@gmail.com>
2016-03-12 11:38:03 +0530

    Updated lein plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,13 @@
-{:user {:plugins [[lein-ancient "0.6.7"]
-                  [lein-exec "0.3.5"]
-                  [lein-pprint "1.1.2"]
-                  [lein-try "0.4.3"]
-                  [refactor-nrepl "1.2.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]
-                  [http-kit/lein-template "1.0.0-SNAPSHOT"]
-                  [compojure-app/lein-template "0.4.7"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.10"]
-                       [compliment "0.2.5"]]}}
+{:user {:plugins
+        [[lein-ancient "0.6.7"]
+         [lein-exec "0.3.5"]
+         [lein-pprint "1.1.2"]
+         [lein-try "0.4.3"]
+         [http-kit/lein-template "1.0.0-SNAPSHOT"]
+         [compojure-app/lein-template "0.4.7"]]}
+ :repl {:plugins
+        [[refactor-nrepl "2.0.0"]
+         [cider/cider-nrepl "0.12.0-SNAPSHOT"]]
+        :dependencies
+        [[org.clojure/tools.nrepl "0.2.12"]
+         [compliment "0.2.5"]]}}
