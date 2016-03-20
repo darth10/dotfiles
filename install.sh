acee02cdd639651fe69186fc07121c51084b960c
Akhil Wali <akhil.wali.10@gmail.com>
2016-03-20 11:27:16 +0530

    Added thinkhdaps submodule. Modified install.sh.

--- install.sh
+++ install.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-sudo apt-get install git emacs24 wicd feh \
-  xscreensaver xscreensaver-gl xscreensaver-gl-extra \
-  i3 x11-xserver-utils xkbset gtk-chtheme lxappearance \
+sudo apt-get install hdapsd tp-smapi-dkms thinkfan xubuntu-restricted-extras \
+  git emacs24 wicd feh \
+  x11-xserver-utils xscreensaver xscreensaver-gl xscreensaver-gl-extra \
+  i3 xkbset gtk-chtheme lxappearance \
   cowsay cmatrix
+
+# install HDAPS indicator
+git submodule init
+git submodule update
+cd thinkhdaps
+./autogen.sh
+./configure
+make
+sudo make install
+git reset --hard
+cd ..
