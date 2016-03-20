8d77f82f24b7aeb834a8805517739b8fe8a311e1
Akhil Wali <akhil.wali.10@gmail.com>
2016-03-20 11:39:44 +0530

    `apt-get install` before copying dotfiles

--- install.sh
+++ install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-rsync -av --progress . ~ \
-      --exclude .git \
-      --exclude .gitmodules \
-      --exclude install.sh \
-      --exclude thinkhdaps
-
 sudo apt-get install hdapsd tp-smapi-dkms thinkfan xubuntu-restricted-extras \
   git emacs24 wicd feh \
   x11-xserver-utils xscreensaver xscreensaver-gl xscreensaver-gl-extra \
   i3 xkbset gtk-chtheme lxappearance \
   cowsay cmatrix
+
+rsync -av --progress . ~ \
+      --exclude .git \
+      --exclude .gitmodules \
+      --exclude install.sh \
+      --exclude thinkhdaps
 
 # install HDAPS indicator
 git submodule init
