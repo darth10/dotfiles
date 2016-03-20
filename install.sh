9c106996f5478be8cbd7f27e2345d89732e3e123
Akhil Wali <akhil.wali.10@gmail.com>
2016-03-20 11:10:06 +0530

    Using i3 window manager

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get install git emacs24 wicd feh \
+  xscreensaver xscreensaver-gl xscreensaver-gl-extra \
+  i3 x11-xserver-utils xkbset gtk-chtheme lxappearance \
+  cowsay cmatrix
