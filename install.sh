8df54299d8ac075fdf776b17802f0f9802ec327d
Akhil Wali <akhil.wali.10@gmail.com>
2018-12-28 22:14:27 +1300

    Use stumpwm.d Makefile

--- install.sh
+++ install.sh
@@ -79,10 +79,8 @@ fi
 # install stumpwm config
 if [ ! -d "~/.stumpwm.d"] then
    git clone git@github.com:darth10/stumpwm.d.git ~/.stumpwm.d
-   ln -s ~/.stumpwm.d/init.lisp ~/.stumpwmrc
    cd ~/.stumpwm.d
-   git submodule init
-   git submodule update
+   make
 fi
 
 # install theme
