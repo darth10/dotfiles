9a79bdf280a9ac22dbbd93016de8ebfef39e41fc
Akhil Wali <akhil.wali.10@gmail.com>
2019-01-03 09:27:31 +1300

    Updated global lein profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,14 +1,14 @@
 {:user {:plugins
-        [[lein-ancient "0.6.10"]
-         [lein-exec "0.3.6"]
-         [lein-pprint "1.1.2"]
+        [[lein-ancient "0.6.15"]
+         [lein-exec "0.3.7"]
+         [lein-pprint "1.2.0"]
          [lein-try "0.4.3"]
          [http-kit/lein-template "1.0.0-SNAPSHOT"]
-         [chestnut/lein-template "0.14.0"]
-         [compojure-app/lein-template "0.4.7"]]}
+         [chestnut/lein-template "0.17.0"]
+         [compojure-app/lein-template "0.4.8"]]}
  :repl {:plugins
-        [[refactor-nrepl "2.3.0-SNAPSHOT"]
-         [cider/cider-nrepl "0.14.0"]]
+        [[refactor-nrepl "2.4.0"]
+         [cider/cider-nrepl "0.20.0-SNAPSHOT"]]
         :dependencies
-        [[org.clojure/tools.nrepl "0.2.12"]
-         [compliment "0.3.2"]]}}
+        [[org.clojure/tools.nrepl "0.2.13"]
+         [compliment "0.3.8"]]}}
