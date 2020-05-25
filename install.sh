e7c257d6665bf3ab799c48a2397741c28f2bfad0
Akhil Wali <akhil.wali.10@gmail.com>
2020-05-25 21:11:35 +1200

    Add Roslyn compiler

--- install.sh
+++ install.sh
@@ -59,6 +59,13 @@ sudo chmod a+rw /etc/stack
 sudo echo 'allow-different-user: true' | sudo tee /etc/stack/config.yaml
 sudo chmod a+rw /etc/stack/config.yaml
 
+# install Mono compiler
+sudo apt install ca-certificates
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+sudo apt update
+sudo apt install mono-roslyn
+
 # install quicklisp
 sbcl --non-interactive --load /usr/share/common-lisp/source/quicklisp/quicklisp.lisp --eval '(quicklisp-quickstart:install :path ".quicklisp/")'
 # start using ~/.quicklisp/setup.lisp
